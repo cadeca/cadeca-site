93bb6961acbd279a1ab212233d6385434cc18ce2
Alexandra Ionel <alexandra.ionel@dialogdata.de>
2020-09-03 13:12:37 +0300

    CDC-0 cadeca-site, contact us

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LandingPageComponent} from './pages/landing-page/landing-page.component';
+import {AboutPageComponent} from './pages/about-page/about-page.component';
+import {ContactPageComponent} from './pages/contact-page/contact-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'landing', component: LandingPageComponent },
+  { path: 'about', component: AboutPageComponent },
+  { path: 'contact', component: ContactPageComponent },
+  { path: '',   redirectTo: '/landingt', pathMatch: 'full' },
+  {
+    path: '**', redirectTo: '/landing'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    AboutPageComponent,
+    ContactPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
